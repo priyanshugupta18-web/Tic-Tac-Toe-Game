7809205a7c2bc2308b069c5b232f981ee7e6a29b
Priyanshu <priyanshuguptawebdev@gmail.com>
2026-04-03 00:52:50 +0530

    full and final

--- SCRIPTS/app.js
+++ SCRIPTS/app.js
@@ -1,6 +1,8 @@
 let boxes = document.querySelectorAll(".boxes");
 let resetbtn = document.querySelector("#reset");
 let newbtn = document.querySelector("#new");
+let para = document.querySelector(".para");
+let gameOver = false;
 
 let turnX = true;
 
@@ -16,53 +18,67 @@ let winPatterns = [
 ]
 
 let isWinner = () => {
+    if (gameOver) return true;
+
     for (let pattern of winPatterns) {
         let a = boxes[pattern[0]].innerText;
         let b = boxes[pattern[1]].innerText;
         let c = boxes[pattern[2]].innerText;
+
         if (a == b && b == c && a != "") {
+            gameOver = true;
+
             document.querySelector(".resetbtn").style.display = "none";
             document.querySelector(".mastercontainer").style.display = "none";
             document.querySelector(".newbtn").style.display = "flex";
+
+            para.innerText = `Congratulations! Player ${a} is the Winner`;
             return true;
         }
     }
     return false;
-}
+};
+
 
 let isDraw = () => {
-    if (isWinner()) {
-        return false;
-    }
+    if (gameOver) return false;
+
     for (let box of boxes) {
         if (box.innerText === "") {
             return false;
         }
     }
+
+    gameOver = true;
+
     document.querySelector(".resetbtn").style.display = "none";
     document.querySelector(".mastercontainer").style.display = "none";
     document.querySelector(".newbtn").style.display = "flex";
+
+    para.innerText = "Its a draw!";
     return true;
-}
+};
+
 
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
-        if (turnX == true) {
+        if (gameOver) return;  
+
+        if (turnX) {
             box.innerText = "X";
             turnX = false;
-            box.disabled = true;
-        }
-        else {
+        } else {
             box.innerText = "O";
             turnX = true;
-            box.disabled = true;
         }
+
+        box.disabled = true;
 
         isWinner();
         isDraw();
+    });
+});
 
-    })
-})
 
 resetbtn.addEventListener("click", () => {
     for (let box of boxes) {
@@ -70,15 +86,20 @@ resetbtn.addEventListener("click", () => {
         box.disabled = false;
     }
     turnX = true;
-})
+    gameOver = false;
+});
+
 
 newbtn.addEventListener("click", () => {
     for (let box of boxes) {
         box.innerText = "";
         box.disabled = false;
     }
+
     turnX = true;
+    gameOver = false;   
+
     document.querySelector(".resetbtn").style.display = "";
     document.querySelector(".mastercontainer").style.display = "";
     document.querySelector(".newbtn").style.display = "none";
-})
+});
